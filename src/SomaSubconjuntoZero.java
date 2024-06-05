ce85ba39a48c6788d59cab5b292a32bd02e29ac8
bmatox <araujobrunomatos@gmail.com>
2024-06-05 01:15:04 -0300

    Updates.

--- src/SomaSubconjuntoZero.java
+++ src/SomaSubconjuntoZero.java
@@ -33,8 +33,8 @@ public class SomaSubconjuntoZero {
     }
 
     private static boolean subconjuntoNaoVazio() {
-        for (boolean incluido : subconjunto) {
-            if (incluido) {
+        for (int i = 0; i < subconjunto.length; i++) {
+            if (subconjunto[i]) {
                 return true;
             }
         }
@@ -65,3 +65,50 @@ public class SomaSubconjuntoZero {
         encontrarSubconjuntoComSomaZero(numerosEntrada);
     }
 }
+
+// pseudocódigo
+
+/*
+procedimento EncontrarSubconjuntoComSomaZero(numerosEntrada : inteiros)
+1.  numeros := numerosEntrada
+2.  subconjunto := novo array booleano[numeros.length]
+3.  solucaoEncontrada := falso
+4.  buscarSubconjunto(0, 0)
+5.  se (!solucaoEncontrada)
+6.      print "Não foi possível encontrar um subconjunto com soma zero."
+
+procedimento buscarSubconjunto(indice : inteiro, somaAtual : inteiro)
+7.  se (indice = numeros.length)
+8.      se (somaAtual = 0 e subconjuntoNaoVazio())
+9.          solucaoEncontrada := verdadeiro
+10.         imprimirSubconjunto()
+11.     retorne
+12. subconjunto[indice] := verdadeiro
+13. buscarSubconjunto(indice + 1, somaAtual + numeros[indice])
+14. subconjunto[indice] := falso
+15. buscarSubconjunto(indice + 1, somaAtual)
+
+função subconjuntoNaoVazio() : booleano
+16. para i ← 0 até subconjunto.length - 1
+17.     se (subconjunto[i])
+18.         retorne verdadeiro
+19. retorne falso
+
+procedimento imprimirSubconjunto()
+20. print "Subconjunto com soma zero encontrado: "
+21. para i ← 0 até numeros.length - 1
+22.     se (subconjunto[i])
+23.         print numeros[i] + " "
+24. print "\n"
+
+procedimento main()
+25. iniciar o programa
+26. print "Digite a quantidade de números: "
+27. quantidade := ler número inteiro
+28. numerosEntrada := novo array int[quantidade]
+29. print "Digite os números (positivos e negativos): "
+30. para i ← 0 até quantidade - 1
+31.     numerosEntrada[i] := ler número inteiro
+32. EncontrarSubconjuntoComSomaZero(numerosEntrada)
+
+    */
