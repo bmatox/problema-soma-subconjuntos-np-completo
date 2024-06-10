c54d8dd00b0bf847dd882aa23347861d0b141f63
bmatox <araujobrunomatos@gmail.com>
2024-06-10 18:52:38 -0300

    Updates.

--- src/SomaSubconjuntoZero.java
+++ src/SomaSubconjuntoZero.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SomaSubconjuntoZero {
     private static int[] numeros; // Array para armazenar os números de entrada
     private static boolean[] subconjunto; // Array para armazenar se um número está no subconjunto ou não
-    private static boolean solucaoEncontrada = false; // Variável para indicar se uma solução foi encontrada ou não
+    private static boolean solucaoEncontrada = false;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -21,7 +21,7 @@ public class SomaSubconjuntoZero {
 
     public static void encontrarSubconjuntoComSomaZero(int[] numerosEntrada) {
         numeros = numerosEntrada;
-        subconjunto = new boolean[numeros.length]; // Inicializa o array de subconjunto com o mesmo tamanho que o array de números
+        subconjunto = new boolean[numeros.length];
         buscarSubconjunto(0, 0);
         if (!solucaoEncontrada) { // Se nenhuma solução foi encontrada, exibe uma mensagem
             System.out.println("Não foi possível encontrar um subconjunto com soma zero.");
@@ -80,13 +80,15 @@ combinações possíveis de números para encontrar um subconjunto cuja soma é 
 ele é impresso; caso contrário, uma mensagem é exibida indicando que não foi possível encontrar um subconjunto com
 soma zero.
 
-No código SomaSubconjuntoZero.java, o backtracking é usado para encontrar um subconjunto de números que somam zero.
+No código o backtracking é usado para encontrar um subconjunto de números que somam zero.
 O algoritmo tenta incluir cada número no subconjunto (primeira chamada recursiva em buscarSubconjunto()) e também
 explora a possibilidade de não incluir o número (segunda chamada recursiva em buscarSubconjunto()).  Se a soma do
 subconjunto for zero, o subconjunto é impresso. Se não, o algoritmo "volta atrás" (daí o termo "backtracking") e
 tenta uma combinação diferente, excluindo o número atual do subconjunto.  O backtracking é evidente na maneira
 como o algoritmo explora todas as combinações possíveis de inclusão e exclusão de números, e como ele "volta atrás"
 se uma combinação particular não leva a uma solução. Isso é feito através das chamadas recursivas em buscarSubconjunto(),
-que permitem ao algoritmo explorar todas as possibilidades e encontrar todas as soluções possíveis.
+que permitem ao algoritmo explorar todas as possibilidades e encontrar todas as soluções possíveis. Embora possa parecer
+que a segunda chamada recursiva duplica o trabalho, na verdade é uma maneira eficiente de percorrer o espaço de solução
+sem a necessidade de gerar todas as combinações de subconjuntos explicitamente.
 
 */
